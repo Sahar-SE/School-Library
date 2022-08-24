0e33b190bdd713f297f6e18b8b25679b6bd1cbd7
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-08-25 02:16:11 +0430

    Add person class

---
+++ person.rb
@@ -0,0 +1,35 @@
+class Person
+    # accessing the attributes by attr_accessor method
+    attr_accessor :name, :age
+    attr_reader :id
+    def initialize(age, name = "Unknown", parent_permission = true)
+    # instance variables
+       @id = Random.rand(1..300)
+       @name = name
+       @age = age 
+       @parent_permission = parent_permission
+    end
+    def is_of_age?
+        age >= 18 ? true : false
+    end
+    
+    private :is_of_age?
+    
+    def can_use_services?
+        age >= 18 or parent_permission ? true : false 
+    end
+
+    public :can_use_services?
+end
+
+class Student < Person
+    def initialize(classroom)
+        super(age, name, parent_permission)
+        @classroom =classroom
+    end
+    def play_hooky
+        "¯\(ツ)/¯"
+    end
+end
+
+
