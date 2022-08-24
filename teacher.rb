9a152404f101af2a43a811ed4df348d64a20b68a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-08-25 02:17:21 +0430

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,9 @@
+class Teacher < Person
+    def initialize(specialization)
+        super(age, name, parent_permission)
+        @specialization = specialization 
+    end
+    def can_use_services?
+        true
+    end
+end
