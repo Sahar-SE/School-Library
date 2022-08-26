339f6f69da0f839466765bf9ef3c05e3462ced34
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-08-26 11:58:48 +0430

    Add classroom

---
+++ classroom.rb
@@ -0,0 +1,12 @@
+class Classroom
+    attr_accessor :lable
+    def initialize(lable)
+        @lable = lable
+        @students = []
+    end
+
+    def add_student(student)
+        @students << student
+        student.classroom = self
+    end
+end
