58ca6299f185b10e2c1f9d48c015f02e2d6ea499
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-09-07 11:12:49 +0100

    Add perserve-data additions

--- students.rb
+++ students.rb
@@ -1,18 +1,30 @@
-require_relative 'person'
-require_relative 'classroom'
+require './person'
+
 class Student < Person
-  def initialize(age, parent_permission, classroom, name = 'unknown')
-    super(age, name, parent_permission: parent_permission)
-    # @classroom = classroom.new(classroom)
+  attr_reader :classroom
+
+  def initialize(age, classroom, parent_permission: true, name: 'Unknown', id: SecureRandom.hex(5))
+    super(age, parent_permission: parent_permission, name: name, id: id)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
 
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students << self unless classroom.students.include?(self)
+  def to_json(*_args)
+    {
+      class: self.class,
+      id: id,
+      age: age,
+      name: name,
+      parent_permission: parent_permission,
+      classroom: classroom
+    }.to_json
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,22 @@
-require_relative 'person'
+require './person'
+
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  attr_accessor :specialization
+
+  def initialize(age, specialization, parent_permission: true, name: 'Unknown', id: SecureRandom.hex(5))
+    super(age, parent_permission: parent_permission, name: name, id: id)
     @specialization = specialization
+  end
+
+  def to_json(*_args)
+    {
+      class: self.class,
+      id: id,
+      age: age,
+      name: name,
+      parent_permission: parent_permission,
+      specialization: specialization
+    }.to_json
   end
 
   def can_use_services?
